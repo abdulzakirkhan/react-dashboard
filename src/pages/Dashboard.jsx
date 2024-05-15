5ebf4bb50621572c21ed12406bd61ea867011b38
Abdul Zakir <abdulzakir632@gmail.com>
2024-05-15 09:42:49 +0500

    Dashboard with Sidebar fully responsive

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Dashboard() {
+  return (
+    <div className="container bg-c w-100">
+    <h1 className="text-bg-danger">Dashboard</h1>
+    </div>
+
+  )
+}
